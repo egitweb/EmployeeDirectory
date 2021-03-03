cb3a031f018be94ea1b8752bf443fd5ce7de019f
Eugenio Espinoza <68721930+egitweb@users.noreply.github.com>
2021-03-02 18:05:38 -0600

    App.js update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+    <div className="banner text-center">
+    <h1><i className="fa fa-address-book" aria-hidden="true" /> Employee Directory</h1>
     </div>
+  </div>
   );
 }
 
